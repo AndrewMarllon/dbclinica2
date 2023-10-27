1dac41d93361ced785e07e254d7f0ceb3dfe9397
AndrewMarllon <andriomarllon7@gmail.com>
2023-10-27 20:54:19 -0300

    adicao de alterar e deletar

--- clinicamedica2py.py
+++ clinicamedica2py.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from datetime import date
 
 conexao = mysql.connector.connect(
     host='localhost',
@@ -6,6 +7,9 @@ conexao = mysql.connector.connect(
     password='root',
     database='bdclinica2',
 )
+
+#//CONSULTA////////////////////////////////////////////////////////////////////////////////
+
 #cursor = conexao.cursor()
 #nome_paciente = str(input("Digite o nome do paciente: "))
 #especialista = int(input("escolha o especialista:"
@@ -30,6 +34,21 @@ conexao = mysql.connector.connect(
 #resultado = cursor.fatchall()
 
 #cursor = conexao.cursor()
+#nome_paciente = "andrew"
+#comando = f'DELETE FROM consultas WHERE nome_paciente = "{nome_paciente}"'
+#cursor.execute(comando)
+#conexao.commit()
+
+#cursor = conexao.cursor()
+#nome_paciente = "mauricio"
+#consulta = 1
+#comando = f'UPDATE consultas SET nome_paciente = "{nome_paciente}" WHERE consulta = "{consulta}"'
+#cursor.execute(comando)
+#conexao.commit()
+
+#//PACIENTE//////////////////////////////////////////////////////////////////////////
+
+#cursor = conexao.cursor()
 #nome_paciente = str(input("digite o nome do paciente: "))
 #idade = int(input("digite a idade do paciente: "))
 #CPF = int(input("digite o cpf: "))
@@ -43,7 +62,70 @@ conexao = mysql.connector.connect(
 #cursor.execute(comando)
 #conexao.commit()
 
+#cursor = conexao.cursor()
+#nome_paciente = "martlon"
+#comando = f'DELETE FROM paciente WHERE nome_paciente = "{nome_paciente}"'
+#cursor.execute(comando)
+#conexao.commit()
 
+#cursor = conexao.cursor()
+#nome_paciente = "ANDREWMBS"
+#idade = 18
+#comando = f'UPDATE paciente SET idade= {idade} WHERE nome_paciente = "{nome_paciente}"'
+#cursor.execute(comando)
+#conexao.commit()
+
+
+#//MEDICO////////////////////////////////////////////////////////////////////////////////////////////////
+
+#cursor = conexao.cursor()
+#nome = str (input("digite o nome do medico: "))
+#especialidade = str (input(" digite a especialidade: "))
+#CRN = int (input("digite o CRN: "))
+#CPF = int (input("digite o CPF: "))
+
+#comando = f'INSERT INTO medico (nome, especialidade,CRN, CPF) VALUES ("{nome}", "{especialidade}",{CRN}, {CPF})'
+#cursor.execute(comando)
+#conexao.commit()
+
+#cursor = conexao.cursor()
+#nome = "selem brandão"
+#comando = f'DELETE FROM medico WHERE nome = "{nome}"'
+#cursor.execute(comando)
+#conexao.commit()
+
+#cursor = conexao.cursor()
+#nome = ""
+#CRN = ""
+#comando = f'UPDATE medico SET CRN = {CRN} WHERE nome = "{nome}"'
+#cursor.execute(comando)
+#conexao.commit()
+
+#//EXAME////////////////////////////////////////////////////////////////////////////////////
+
+#cursor = conexao.cursor()
+#nome = str(input("digte o nome do paciente: "))
+#CPF = int(input("digite o CPF: "))
+#exame = str(input("exame: "))
+#data = str(input("data do exame: "))
+#medico = str(input(" medico: "))
+#comando = f'INSERT INTO exames (nome, CPF, exame, data, medico) VALUES ("{nome}", {CPF}, "{exame}", {data}, "{medico}")'
+
+#cursor.execute(comando)
+#conexao.commit()
+
+#cursor = conexao.cursor()
+#nome = "andrew"
+#comando = f'DELETE FROM exames WHERE nome = "{nome}"'
+#ursor.execute(comando)
+#conexao.commit()
+
+#cursor = conexao.cursor()
+#nome = ""
+#medico = ""
+#comando = f'UPDATE exames SET nome = {nome} WHERE medico = "{medico}"'
+#cursor.execute(comando)
+#conexao.commit()
 
 cursor.close()
 conexao.close()
